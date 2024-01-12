b59c5a0a79e06b484cd1bcb5dc618b09c3f53897
Gonzalo Marcos <gmansoain@gmail.com>
2024-01-12 15:57:01 +0000

    Added fetching data and counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
+    // setCount(count + 1);
+    setCount((c) => c + 1);
   }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
   return (
     <div>
-      <h1>Hello Gon</h1>
+      <h1>{advice}</h1>
       <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
     </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pieces of advice
+    </p>
   );
 }
 
